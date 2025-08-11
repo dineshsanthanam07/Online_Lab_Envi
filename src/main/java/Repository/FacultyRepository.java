dee3d2072b2c79c2351863961879cbfd217a7ee1
dineshsanthanam07 <2023178048.student@annauniv.edu>
2025-08-11 21:19:57 +0530

    faculty repository created

---
+++ src/main/java/Repository/FacultyRepository.java
@@ -0,0 +1,11 @@
+package Repository;
+
+import entity.Faculty;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FacultyRepository extends R2dbcRepository<Faculty, Long> {
+
+}
