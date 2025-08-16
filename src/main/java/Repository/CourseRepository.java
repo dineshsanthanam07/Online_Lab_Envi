e5fbb68a1a443828a942b93470855b65b14a83f7
dineshsanthanam07 <2023178048.student@annauniv.edu>
2025-08-16 19:39:46 +0530

    Course service - methods are created

--- src/main/java/Repository/CourseRepository.java
+++ src/main/java/Repository/CourseRepository.java
@@ -1,10 +1,15 @@
 package Repository;
 
 import entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourseRepository extends R2dbcRepository<Course, String> {
+    Page<Course> findAll(Pageable pageable) ;
+
+
 
 }
---
+++ src/main/java/services/Courseservice.java
@@ -0,0 +1,38 @@
+package services;
+
+import Repository.CourseRepository;
+import entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class Courseservice {
+
+private final CourseRepository courseRepository;
+
+    public Courseservice(CourseRepository courseRepository){
+        this.courseRepository=courseRepository;
+    }
+
+
+
+    public Page<Course> getAllCourses(int page,int size){
+        return courseRepository.findAll(PageRequest.of(page, size));
+    }
+
+    public Mono<Course> getCourse(String course_id){
+        return courseRepository.findById(course_id);
+    }
+
+    public Mono<Course> saveCourse(Course course){
+        return courseRepository.save(course);
+    }
+
+
+    public void deleteCourse(String course_id){
+        courseRepository.deleteById(course_id);
+    }
+
+}
